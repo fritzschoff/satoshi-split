948baa67a0316ee63c435ad51f32a0ac3cf6668e
fritzschoff <fritzschoff@icloud.com>
2025-10-12 21:04:40 +0100

    added config and env example file

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,17 +1,17 @@
-import type { HardhatUserConfig } from "hardhat/config";
-
-import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
-import { configVariable } from "hardhat/config";
+import type { HardhatUserConfig } from 'hardhat/config';
+import hardhatVerify from '@nomicfoundation/hardhat-verify';
+import hardhatToolboxViemPlugin from '@nomicfoundation/hardhat-toolbox-viem';
+import { configVariable } from 'hardhat/config';
 
 const config: HardhatUserConfig = {
-  plugins: [hardhatToolboxViemPlugin],
+  plugins: [hardhatToolboxViemPlugin, hardhatVerify],
   solidity: {
     profiles: {
       default: {
-        version: "0.8.28",
+        version: '0.8.28',
       },
       production: {
-        version: "0.8.28",
+        version: '0.8.28',
         settings: {
           optimizer: {
             enabled: true,
@@ -23,18 +23,23 @@ const config: HardhatUserConfig = {
   },
   networks: {
     hardhatMainnet: {
-      type: "edr-simulated",
-      chainType: "l1",
+      type: 'edr-simulated',
+      chainType: 'l1',
     },
     hardhatOp: {
-      type: "edr-simulated",
-      chainType: "op",
+      type: 'edr-simulated',
+      chainType: 'op',
     },
     sepolia: {
-      type: "http",
-      chainType: "l1",
-      url: configVariable("SEPOLIA_RPC_URL"),
-      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+      type: 'http',
+      chainType: 'l1',
+      url: configVariable('SEPOLIA_RPC_URL'),
+      accounts: [configVariable('SEPOLIA_PRIVATE_KEY')],
+    },
+  },
+  verify: {
+    etherscan: {
+      apiKey: configVariable('ETHERSCAN_API_KEY'),
     },
   },
 };
